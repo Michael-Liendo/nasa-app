7cf873f1fe75082769432dd128e078135d21a547
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-06-18 15:54:22 -0400

    refactor: rename and update paths

--- src/pages/mars.js
+++ src/pages/mars.js
@@ -1,6 +1,6 @@
 import Layout from '../components/Layout';
 import { MarsPhotos } from '../lib/nasa';
-import ImageCard from '../components/ImageCard';
+import MarsCard from '../components/MarsCard';
 
 export default function Mars({ marsPhotos }) {
   return (
@@ -8,7 +8,7 @@ export default function Mars({ marsPhotos }) {
       <h1 className="text-5xl text-white text-center">Today Mars Photos</h1>
       <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-2 lg:max-w-none">
         {marsPhotos.map((data) => (
-          <ImageCard key={data.id} data={data} />
+          <MarsCard key={data.id} data={data} />
         ))}
       </div>
     </Layout>
